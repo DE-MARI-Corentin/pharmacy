811a57aa67e8b9e110c7d613f05732d07578136c
cdemari <cdemari@DEVICE-CDEMARI-.local>
2021-12-16 14:21:56 +0100

    Update .eslintrc.cjs, .gitignore, and 43 more files...

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import Navbar from '$lib/header/Navbar.svelte'
+	import Sidebar from '$lib/header/Sidebar.svelte'
+	import Main from '$lib/header/Main.svelte'
+	import '../app.css';
+	let open = false
+</script>
+
+<Sidebar bind:open />
+<Navbar bind:sidebar={open}/>
+<Main/>
+
+<main>
+	<div class="max-w-4xl mx-auto py-8">
+		<slot />
+	</div>
+</main>
+
+<footer>
+	<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
+</footer>
---
+++ tailwind.config.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  // add this section
+  purge: [
+    './src/**/*.html',
+    './src/**/*.svelte'
+  ],
+  media: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
